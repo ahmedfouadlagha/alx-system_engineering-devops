c708ab3159de0c1e3964ca3de5bfb34fdb7a9d3b
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-01-27 21:45:59 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # Install an especific version of flask (2.1.0)
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',  # Update this version to the one compatible with Flask 2.1.0
   provider => 'pip3',
 }
