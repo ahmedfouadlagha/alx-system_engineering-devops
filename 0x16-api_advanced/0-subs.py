765480613b6abdf4017430a6f10ab108e85cc5d9
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-03-15 13:45:53 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
+"""
+    Uses Reddit API to print the number of subscribers of a subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
+    """
+    Get the number of subscribers for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
-    json = r.json()
-    data_dict = json.get('data')
-    return(data_dict.get('subscribers'))
+
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+
+    return num_subs
