0245a59113f97695ab3b63b90271d660856e0081
Ahmed F. LAGHA <laghaahmedfouad@gmail.com>
2024-08-13 17:40:01 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """ Script that returns no. of subscribers of a subreddit """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Function that returns no. of subs of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
