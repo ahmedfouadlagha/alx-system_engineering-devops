180ecf62c0f70a5b9777048111e8eb9072fcf430
Ahmed F. LAGHA <laghaahmedfouad@gmail.com>
2024-08-13 16:58:14 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """A script that gets the number of subscribers for a subreddit"""
-import json
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Function that requests the number of subscribers for a subreddit"""
-
+    
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     USER_AGENT = "com.holbertonschool.myredditscript:0.0.1 (by /u/dmaring)"
     headers = {'User-Agent': USER_AGENT}
-    r = requests.get(URL, headers=headers)
-    if (r.status_code is not 200):
-        return(0)
-    r = r.json()
-    return(r['data']['subscribers'])
+    
+    # Making the request to the Reddit API
+    r = requests.get(URL, headers=headers, allow_redirects=False)
+    
+    # Check if the request was successful and not redirected
+    if r.status_code != 200:
+        return 0
+    
+    # Parse the response as JSON
+    try:
+        r = r.json()
+        return r['data']['subscribers']
+    except (KeyError, ValueError):
+        return 0
