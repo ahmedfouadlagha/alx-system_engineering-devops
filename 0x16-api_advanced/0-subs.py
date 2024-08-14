4a5be0bb2afb4c30d866072e064df80b4d0d3982
Ahmed F. LAGHA <laghaahmedfouad@gmail.com>
2024-08-14 12:15:55 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-""" Script that returns no. of subscribers of a subreddit """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Function that returns no. of subs of a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers={'User-Agent': 'selBot/1.0'})
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    # Set a custom User-Agent to avoid Too Many Requests errors
+    headers = {'User-Agent': 'Custom User-Agent for subreddit queries'}
+    # Define the API URL
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    try:
+        # Make a GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            # Return the number of subscribers
+            return data['data']['subscribers']
+        else:
+            # If subreddit is invalid, return 0
+            return 0
+    except Exception:
+        # In case of any exceptions, return 0
         return 0
