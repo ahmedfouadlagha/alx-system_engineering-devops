685f5c6d2fb2e00142e7458a5ef82895091cccab
Ahmed F. LAGHA <laghaahmedfouad@gmail.com>
2024-08-19 18:01:27 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-''' task 1 module'''
-
+"""Script that returns top 10 hot posts of a subreddit"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    '''gets 10 hottest posts of a subreddit'''
-    headers = {'User-agent': 'test23'}
-    url = 'https://www.reddit.com/r/'
-    posts = requests.get(url + '{}/hot.json?limit=10'.format(
-        sys.argv[1]), allow_redirects=False, headers=headers)
+    """Function that prints top 10 posts of a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
+    data = response.json()
 
-    if posts.status_code == 200:
-        for post in posts.json()['data']['children']:
+    if response.status_code == 200:
+        posts = data['data']['children']
+        for post in posts:
             print(post['data']['title'])
     else:
         print(None)
